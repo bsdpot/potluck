f1dc5ab049e273d4ad8db2cfc6a4596a2048cf49
hny-gd <65355462+hny-gd@users.noreply.github.com>
2020-06-22 16:46:23 +0200

    Add files via upload
    Added flavour "firefox"l

---
+++ firefox/firefox.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+[ -w /etc/pkg/FreeBSD.conf ] && sed -i '' 's/quarterly/latest/' /etc/pkg/FreeBSD.conf
+ASSUME_ALWAYS_YES=yes pkg bootstrap
+touch /etc/rc.conf
+sysrc sendmail_enable="NONE"
+sysrc sshd_enable="YES"
+
+echo myjailpassword | pw add user ffoxuser -h 0
+mkdir /home/ffoxuser
+chown ffoxuser /home/ffoxuser
+
+pkg install -y xauth firefox 
+pkg clean -y
