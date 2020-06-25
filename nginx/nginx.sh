43ac5d086646673100c449ca92ff83ca9866c8c8
hny-gd <65355462+hny-gd@users.noreply.github.com>
2020-06-25 09:11:48 +0200

    Add files via upload
    Added NGINX

---
+++ nginx/nginx.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+[ -w /etc/pkg/FreeBSD.conf ] && sed -i '' 's/quarterly/latest/' /etc/pkg/FreeBSD.conf
+ASSUME_ALWAYS_YES=yes pkg bootstrap
+touch /etc/rc.conf
+sysrc sendmail_enable="NO"
+sysrc nginx_enable="YES"
+pkg install -y nginx
+echo "error_log /dev/stderr;" >> /usr/local/etc/nginx/nginx.conf
+sed -i '' 's%#access_log  logs/access.log .*$%access_log /dev/stdout combined;%' /usr/local/etc/nginx/nginx.conf
+pkg clean -y
