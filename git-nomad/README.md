447a8cd08a1eb4d278a933645b72a8f2811eb50d
Honeyguide <65355462+hny-gd@users.noreply.github.com>
2020-07-28 10:00:10 +0200

    Add files via upload
    Added git-nomad jail

---
+++ git-nomad/README.md
@@ -0,0 +1,74 @@
+---
+author: "Stephan Lichtenauer"
+title: Git (Nomad)
+summary: This is a Git server jail that can be deployed via nomad.
+tags: ["git", "nomad"]
+---
+
+# Overview
+
+This is a Git jail that can be started with ```pot``` but it can also be deployed via ```nomad```.
+
+The git user home directory is in ```/var/db/git``` where also a directory ```.ssh``` for authorized key access resides.    
+It is suggested that this directory is mounted from outside the jail when it is run as a ```nomad``` task so that it is persistent (see example below).
+
+Git is started as blocking task when the jail is started (see ```git-nomad+4.sh```).
+
+For more details about ```nomad```images, see [about potluck](https://potluck.honeyguide.net/micro/about-potluck/).
+
+# Nomad Job Description Example
+
+It is suggested to mount the jail directory ```/var/db/git``` from outside as this contains the git database and the ```.ssh/authorized_keys``` file for access:
+
+```
+job "examplegit" {
+  datacenters = ["datacenter"]
+  type        = "service"
+
+  group "group1" {
+    count = 1 
+
+    task "www1" {
+      driver = "pot"
+
+      service {
+        tags = ["git"]
+        name = "git-example-service"
+        port = "ssh"
+
+         check {
+            type     = "tcp"
+            name     = "tcp"
+            interval = "60s"
+            timeout  = "30s"
+          }
+      }
+
+      config {
+        image = "https://potluck.honeyguide.net/git-nomad"
+        pot = "git-nomad-amd64-12_1"
+        tag = "1.0"
+        command = "/usr/local/bin/cook"
+        args = [""]
+
+       mount = [
+         "/mnt/s3/web/git:/var/db/git"
+       ]
+        port_map = {
+          http = "22"
+        }
+      }
+
+      resources {
+        cpu = 200
+        memory = 64
+
+        network {
+          mbits = 10
+          port "ssh" {}
+        }
+      }
+    }
+  }
+}
+```
---
+++ git-nomad/git-nomad.sh
@@ -0,0 +1,133 @@
+#!/bin/sh
+
+# POTLUCK TEMPLATE v1.0
+# EDIT THE FOLLOWING FOR NEW FLAVOUR:
+# 1. RUNS_IN_NOMAD - yes or no
+# 2. Adjust package installation between BEGIN & END PACKAGE SETUP
+# 3. Adjust jail configuration script generation between BEGIN & END COOK
+
+# Set this to true if this jail flavour is to be created as a nomad (i.e. blocking) jail.
+# You can then query it in the cook script generation below and the script is installed
+# appropriately at the end of this script 
+RUNS_IN_NOMAD=true
+
+# -------------- BEGIN PACKAGE SETUP -------------
+[ -w /etc/pkg/FreeBSD.conf ] && sed -i '' 's/quarterly/latest/' /etc/pkg/FreeBSD.conf
+ASSUME_ALWAYS_YES=yes pkg bootstrap
+touch /etc/rc.conf
+sysrc sendmail_enable="NO"
+sysrc sshd_enable="YES"
+
+# Install packages
+pkg install -y git 
+pkg clean -y
+# -------------- END PACKAGE SETUP -------------
+
+#
+# Create configurations
+#
+
+#
+# Now generate the run command script "cook"
+# It configures the system on the first run by creating the config file(s) 
+# On subsequent runs, it only starts sleeps (if nomad-jail) or simply exits 
+#
+
+# ----------------- BEGIN COOK ------------------ 
+echo "#!/bin/sh
+RUNS_IN_NOMAD=$RUNS_IN_NOMAD
+# No need to change this, just ensures configuration is done only once
+if [ -e /usr/local/etc/pot-is-seasoned ]
+then
+    # If this pot flavour is blocking (i.e. it should not return), 
+    # we block indefinitely
+    if [ \$RUNS_IN_NOMAD ]
+    then
+        tail -f /dev/null 
+    fi
+    exit 0
+fi
+
+# ADJUST THIS: STOP SERVICES AS NEEDED BEFORE CONFIGURATION
+
+# No need to adjust this:
+# If this pot flavour is not blocking, we need to read the environment first from /tmp/environment.sh
+# where pot is storing it in this case
+if [ -e /tmp/environment.sh ]
+then
+    . /tmp/environment.sh
+fi
+
+#
+# ADJUST THIS BY CHECKING FOR ALL VARIABLES YOUR FLAVOUR NEEDS:
+# Check config variables are set
+#
+
+# ADJUST THIS BELOW: NOW ALL THE CONFIGURATION FILES NEED TO BE CREATED:
+# Don't forget to double(!)-escape quotes and dollar signs in the config files
+# Create traefik server config file 
+
+pw user add -n git -c 'Git Server' -d /var/db/git -G wheel -m -s /usr/sbin/nologin
+mkdir -p /var/db/git/.ssh
+chown git:git /var/db/git/.ssh
+touch /var/db/git/.ssh/authorized_keys
+chown git:git /var/db/git/.ssh/authorized_keys
+chmod u+rw /var/db/git/.ssh/authorized_keys
+chmod go-w /var/db/git/.ssh/authorized_keys
+
+# ADJUST THIS: START THE SERVICES AGAIN AFTER CONFIGURATION
+
+# Do not touch this:
+touch /usr/local/etc/pot-is-seasoned
+# If this pot flavour is blocking (i.e. it should not return), there is no /tmp/environment.sh
+# created by pot and we now after configuration block indefinitely
+if [ \$RUNS_IN_NOMAD ]
+then
+    tail -f /dev/null
+fi
+" > /usr/local/bin/cook
+
+# ----------------- END COOK ------------------
+
+
+# ---------- NO NEED TO EDIT BELOW ------------
+
+chmod u+x /usr/local/bin/cook
+
+#
+# There are two ways of running a pot jail: "Normal", non-blocking mode and
+# "Nomad", i.e. blocking mode (the pot start command does not return until
+# the jail is stopped).
+# For the normal mode, we create a /usr/local/etc/rc.d script that starts
+# the "cook" script generated above each time, for the "Nomad" mode, the cook
+# script is started by pot (configuration through flavour file), therefore
+# we do not need to do anything here.
+# 
+
+# Create rc.d script for "normal" mode:
+echo "#!/bin/sh
+#
+# PROVIDE: cook 
+# REQUIRE: LOGIN
+# KEYWORD: shutdown
+#
+. /etc/rc.subr
+name=cook
+rcvar=cook_enable
+load_rc_config $name
+: ${cook_enable:=\"NO\"}
+: ${cook_env:=\"\"}
+command=\"/usr/local/bin/cook\"
+command_args=\"\"
+run_rc_command \"\$1\"
+" > /usr/local/etc/rc.d/cook
+
+chmod u+x /usr/local/etc/rc.d/cook
+
+if [ $RUNS_IN_NOMAD = false ]
+then
+    # This is a non-nomad (non-blocking) jail, so we need to make sure the script
+    # gets started when the jail is started:
+    # Otherwise, /usr/local/bin/cook will be set as start script by the pot flavour
+    echo "cook_enable=\"YES\"" >> /etc/rc.conf
+fi
