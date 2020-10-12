fcfe22896a3c568c7901188cf43f08ba5e6b76d7
Honeyguide <65355462+hny-gd@users.noreply.github.com>
2020-10-12 10:52:27 +0200

    ELK v1.0 (Elasticsearch/Logstash/Kibana)

---
+++ elk-stack-nomad/README.md
@@ -0,0 +1,84 @@
+---
+author: "Stephan Lichtenauer"
+title: ELK Stack (Nomad)
+summary: This is an Elasticsearch/Logstash/Kibana jail that can be deployed via nomad.
+tags: ["elasticsearch", "logstash", "kibana", "monitoring", "nomad"]
+---
+
+# Overview
+
+This is a ```elasticsearch```/```logstash```/```kibana``` (ELK)  jail that can be started with ```pot``` but it can also be deployed via ```nomad```.
+
+The ```elasticsearch``` storage directory is in ```/var/db/elasticsearch```.    
+It is suggested that this directory is mounted from outside the jail when it is run as a ```nomad``` task so that it is persistent (see example below).
+
+The ```logstash``` configuration (```logstash.yml```, ```logstash.conf```, ```pipelines.yml```) is located in ```/usr/local/etc/logstash``` and you probably want to copy in your own configuration and potentially mount some source directories that can be accessed depending on your monitoring and aggregation needs.
+
+The kibana web interface can be accessed via web browser through port 5601. 
+
+For more details about ```nomad```images, see [about potluck](https://potluck.honeyguide.net/micro/about-potluck/).
+
+
+# Nomad Job Description Example
+
+```
+job "elkstack" {
+  datacenters = ["mydc"]
+  type        = "service"
+
+  group "group1" {
+    count = 1 
+
+    task "www1" {
+      driver = "pot"
+
+      service {
+        tags = ["elkstack", "www"]
+        name = "elkstack"
+        port = "http"
+
+         check {
+            type     = "tcp"
+            name     = "tcp"
+            interval = "60s"
+            timeout  = "30s"
+          }
+          check_restart {
+            limit = 5
+            grace = "120s"
+            ignore_warnings = false
+          }
+      }
+
+      config {
+        image = "https://potluck.honeyguide.net/elk-stack-nomad"
+        pot = "elk-stack-nomad-amd64-12_1"
+        tag = "1.0"
+        command = "/usr/local/bin/cook"
+        mount = [
+          "/mnt/logfiles:/mnt"
+        ]
+        copy = [
+          "/mnt/logstash.conf:/usr/local/etc/logstash/logstash.conf",
+          "/mnt/logstash.yml:/usr/local/etc/logstash/logstash.yml"
+        ]
+        port_map = {
+          http = "5601"
+        }
+      }
+
+      resources {
+        cpu = 1000
+        memory = 2048 
+
+        network {
+          mbits = 50
+          port "http" {}
+        }
+      }
+    }
+  }
+}
+```
+
+
---
+++ elk-stack-nomad/elk-stack-nomad.sh
@@ -0,0 +1,163 @@
+#!/bin/sh
+
+# POTLUCK TEMPLATE v2.0
+# EDIT THE FOLLOWING FOR NEW FLAVOUR:
+# 1. RUNS_IN_NOMAD - yes or no
+# 2. Create a matching <flavour> file with this <flavour>.sh file that
+#    contains the copy-in commands for the config files from <flavour>.d/
+#    Remember that the package directories don't exist yet, so likely copy to /root
+# 3. Adjust package installation between BEGIN & END PACKAGE SETUP
+# 4. Adjust jail configuration script generation between BEGIN & END COOK
+#    Configure the config files that have been copied in where necessary
+
+# Set this to true if this jail flavour is to be created as a nomad (i.e. blocking) jail.
+# You can then query it in the cook script generation below and the script is installed
+# appropriately at the end of this script 
+RUNS_IN_NOMAD=true
+
+# -------- BEGIN PACKAGE & MOUNTPOINT SETUP -------------
+[ -w /etc/pkg/FreeBSD.conf ] && sed -i '' 's/quarterly/latest/' /etc/pkg/FreeBSD.conf
+ASSUME_ALWAYS_YES=yes pkg bootstrap
+touch /etc/rc.conf
+sysrc sendmail_enable="NO"
+
+# Install packages
+pkg install -y elasticsearch7 logstash7 kibana7 
+pkg clean -y
+
+# Create mountpoints
+# ---------- END PACKAGE & MOUNTPOINT SETUP -------------
+
+#
+# Create configurations
+#
+
+#
+# Now generate the run command script "cook"
+# It configures the system on the first run by creating the config file(s) 
+# On subsequent runs, it only starts sleeps (if nomad-jail) or simply exits 
+#
+
+# ----------------- BEGIN COOK ------------------ 
+echo "#!/bin/sh
+RUNS_IN_NOMAD=$RUNS_IN_NOMAD
+# No need to change this, just ensures configuration is done only once
+if [ -e /usr/local/etc/pot-is-seasoned ]
+then
+    # If this pot flavour is blocking (i.e. it should not return), 
+    # we block indefinitely
+    if [ \$RUNS_IN_NOMAD ]
+    then
+        /bin/sh /etc/rc
+        tail -f /dev/null 
+    fi
+    exit 0
+fi
+
+# ADJUST THIS: STOP SERVICES AS NEEDED BEFORE CONFIGURATION
+
+
+# No need to adjust this:
+# If this pot flavour is not blocking, we need to read the environment first from /tmp/environment.sh
+# where pot is storing it in this case
+if [ -e /tmp/environment.sh ]
+then
+    . /tmp/environment.sh
+fi
+
+#
+# ADJUST THIS BY CHECKING FOR ALL VARIABLES YOUR FLAVOUR NEEDS:
+# 
+
+# Convert parameters to variables if passed (overwrite environment)
+#while getopts a:b:d:g: option
+#do
+#    case \"\${option}\"
+#    in
+#      a) ALPHA=\${OPTARG};;
+#      b) BETA=\${OPTARG};;
+#      d) DELTA=\${OPTARG};;
+#      g) GAMMA=\${OPTARG};;
+#    esac
+#done
+
+# Check config variables are set
+#if [ -z \${ALPHA+x} ]; 
+#then 
+#    echo 'ALPHA is unset - see documentation how to configure this flavour' >> /var/log/cook.log
+#    echo 'ALPHA is unset - see documentation how to configure this flavour'
+#    exit 1
+#fi
+
+# ADJUST THIS BELOW: NOW ALL THE CONFIGURATION FILES NEED TO BE ADJUSTED & COPIED:
+hostip=\$(ifconfig | sed -n '/.inet /{s///;s/ .*//;p;}' | sed -r 's/^127.0.0.1//' | tr -d '\\n')
+cp /usr/local/etc/kibana/kibana.yml.sample /usr/local/etc/kibana/kibana.yml
+echo \"server.host: \\\"\$hostip\\\"\" >> /usr/local/etc/kibana/kibana.yml
+echo \"elasticsearch.hosts: [\\\"http://127.0.0.1:9200\\\"]\" >> /usr/local/etc/kibana/kibana.yml
+
+
+sysrc elasticsearch_enable=YES
+sysrc logstash_enable=YES
+sysrc kibana_enable=YES
+
+
+# ADJUST THIS: START THE SERVICES AGAIN AFTER CONFIGURATION
+/usr/local/etc/rc.d/elasticsearch start
+/usr/local/etc/rc.d/logstash start
+/usr/local/etc/rc.d/kibana start
+
+# Do not touch this:
+touch /usr/local/etc/pot-is-seasoned
+# If this pot flavour is blocking (i.e. it should not return), there is no /tmp/environment.sh
+# created by pot and we now after configuration block indefinitely
+if [ \$RUNS_IN_NOMAD ]
+then
+    /bin/sh /etc/rc
+    tail -f /dev/null
+fi
+" > /usr/local/bin/cook
+
+# ----------------- END COOK ------------------
+
+
+# ---------- NO NEED TO EDIT BELOW ------------
+
+chmod u+x /usr/local/bin/cook
+
+#
+# There are two ways of running a pot jail: "Normal", non-blocking mode and
+# "Nomad", i.e. blocking mode (the pot start command does not return until
+# the jail is stopped).
+# For the normal mode, we create a /usr/local/etc/rc.d script that starts
+# the "cook" script generated above each time, for the "Nomad" mode, the cook
+# script is started by pot (configuration through flavour file), therefore
+# we do not need to do anything here.
+# 
+
+# Create rc.d script for "normal" mode:
+echo "#!/bin/sh
+#
+# PROVIDE: cook 
+# REQUIRE: LOGIN
+# KEYWORD: shutdown
+#
+. /etc/rc.subr
+name=cook
+rcvar=cook_enable
+load_rc_config $name
+: ${cook_enable:=\"NO\"}
+: ${cook_env:=\"\"}
+command=\"/usr/local/bin/cook\"
+command_args=\"\"
+run_rc_command \"\$1\"
+" > /usr/local/etc/rc.d/cook
+
+chmod u+x /usr/local/etc/rc.d/cook
+
+if [ $RUNS_IN_NOMAD = false ]
+then
+    # This is a non-nomad (non-blocking) jail, so we need to make sure the script
+    # gets started when the jail is started:
+    # Otherwise, /usr/local/bin/cook will be set as start script by the pot flavour
+    echo "cook_enable=\"YES\"" >> /etc/rc.conf
+fi
