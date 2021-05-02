d6d1d5604b9b1728b09a03c628096ac5f1d774ba
Honeyguide <65355462+hny-gd@users.noreply.github.com>
2021-05-02 20:01:47 +0200

    Change from latest to quarterly packages

--- nginx-nomad/nginx-nomad.sh
+++ nginx-nomad/nginx-nomad.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-[ -w /etc/pkg/FreeBSD.conf ] && sed -i '' 's/quarterly/latest/' /etc/pkg/FreeBSD.conf
 ASSUME_ALWAYS_YES=yes pkg bootstrap
 touch /etc/rc.conf
 sysrc sendmail_enable="NONE"
