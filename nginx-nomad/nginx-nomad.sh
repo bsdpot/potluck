c414e78ce43c4fb4742a3da8bdb060ec3440c66f
Stephan <65355462+hny-gd@users.noreply.github.com>
2021-05-26 17:40:31 +0200

    Fix sendmail disable

--- nginx-nomad/nginx-nomad.sh
+++ nginx-nomad/nginx-nomad.sh
@@ -2,7 +2,7 @@
 
 ASSUME_ALWAYS_YES=yes pkg bootstrap
 touch /etc/rc.conf
-sysrc sendmail_enable="NONE"
+service sendmail onedisable
 pkg install -y nginx
 echo "error_log /dev/stderr;" >> /usr/local/etc/nginx/nginx.conf
 sed -i '' 's%#access_log  logs/access.log .*$%access_log /dev/stdout combined;%' /usr/local/etc/nginx/nginx.conf
