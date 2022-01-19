aca749b7243b4723836e07871da54f0b78f581c2
Michael Gmelin <freebsd@grem.de>
2022-01-19 19:06:24 +0100

    Reload patroni on consul cert change
    This was done before as well, need some better way to
    synchronize though to avoid clashing.

--- prometheus/prometheus.d/local/share/cook/bin/reload-vaultproxy.sh
+++ prometheus/prometheus.d/local/share/cook/bin/reload-vaultproxy.sh
@@ -4,5 +4,7 @@ cat /mnt/certs/ca.crt /mnt/certs/ca_root.crt \
 mv /mnt/certs/ca_chain.crt.tmp /mnt/certs/ca_chain.crt
 
 service nginx reload vaultproxy
+service prometheus reload
+service alertmanager reload
 
 exit 0
