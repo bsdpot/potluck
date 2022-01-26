afcaf9d51068666d1d1a91a234cbe44658b91829
Michael Gmelin <freebsd@grem.de>
2022-01-26 10:34:54 +0100

    Clean up patroni reload mechanism
    This should prevent patroni from being in undefined states.
    Using sighup should only do local reloads (which is what we want).
    Cleanup rc script.
    
    If patroni turns out to crash (which seems unlikely), we can
    always put it in a dedicated service scanner.

--- postgresql-patroni/postgresql-patroni.d/local/share/cook/bin/reload-patroni.sh
+++ postgresql-patroni/postgresql-patroni.d/local/share/cook/bin/reload-patroni.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
-cat /mnt/postgrescerts/postgresca.crt /mnt/postgrescerts/ca_root.crt \
-  >/mnt/postgrescerts/ca_chain.crt.tmp
-chown postgres /mnt/postgrescerts/*
-mv /mnt/postgrescerts/ca_chain.crt.tmp /mnt/postgrescerts/ca_chain.crt
 
-# restart gives error with port already in use when using this
-#  /usr/local/etc/rc.d/patroni restart
-# so we're using this instead
-/usr/local/bin/patronictl -c /usr/local/etc/patroni/patroni.yml reload \
-  postgresql --force
+lockf -t 20 /tmp/reload-patroni.lock sh -c '
+  set -e
+  cp -a /mnt/postgrescerts/postgresserver.key.next \
+    /mnt/postgrescerts/postgresserver.key
+  cp -a /mnt/postgrescerts/postgresserver.crt.next \
+    /mnt/postgrescerts/postgresserver.crt
+  cp -a /mnt/postgrescerts/postgresca.crt.next /mnt/postgrescerts/postgresca.crt
+  cat /mnt/postgrescerts/postgresca.crt /mnt/postgrescerts/ca_root.crt \
+    >/mnt/postgrescerts/ca_chain.crt.tmp
+  chown postgres /mnt/postgrescerts/*
+  mv /mnt/postgrescerts/ca_chain.crt.tmp /mnt/postgrescerts/ca_chain.crt
+  service patroni reload'
+
 exit 0
