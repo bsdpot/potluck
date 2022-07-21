4029a7460b237b17610892e241928e56c7e049cd
Michael Gmelin <freebsd@grem.de>
2022-07-21 13:35:11 +0200

    consul-tls: Improve static DNS setup during provisioning

--- consul-tls/consul-tls.d/local/share/cook/bin/setup-local-unbound-static.sh
+++ consul-tls/consul-tls.d/local/share/cook/bin/setup-local-unbound-static.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ $# -ne 1 ]; then
+  2>&1 echo "Usage $0 vault_ip"
+  exit 1
+fi
+
 # shellcheck disable=SC1091
 . /root/.env.cook
 
@@ -13,6 +18,8 @@ echo "server:
         local-zone: active.vault.service.consul typetransparent
         local-data: \"active.vault.service.consul A $1\"
 " >/etc/unbound/conf.d/vault-static.conf
+
+rm -f /etc/unbound/conf.d/consul.conf
 
 service local_unbound enable || true
 service local_unbound restart || true
