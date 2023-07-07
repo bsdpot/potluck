4ba67964a303a3b82129f1ccdef688c7ac1ae378
Bretton Vine <bretton@hivemind.net>
2023-07-07 19:42:14 +0200

    Version bump for rebuild
    use netrc file with curl for index creation

--- dmarc-report/dmarc-report.d/local/share/cook/bin/configure-parsedmarc.sh
+++ dmarc-report/dmarc-report.d/local/share/cook/bin/configure-parsedmarc.sh
@@ -29,43 +29,54 @@ chown -R parsedmarc "/mnt/$OUTPUTFOLDER"
 # 'core.MultiSearchV2: error accessing reader: no index found'
 # Any index will do, just needs to be an existing index before parsedmarc will import correctly
 
-# check zincsearch responding
-zinclivecheck=$(/usr/local/bin/curl -s "http://localhost:9200/" | jq -r .name )
-
 # copy empty index json files to /tmp (might be redundant)
 cp -f "$TEMPLATEPATH/dmarc_aggregate.json.in" /tmp/dmarc_aggregate.json
 cp -f "$TEMPLATEPATH/dmarc_forensic.json.in" /tmp/dmarc_forensic.json
 
 # set credentials
-mycreds="$ZINCUSER:$ZINCPASS"
+echo "machine localhost login ${ZINCUSER} password ${ZINCPASS}" > /root/.netrc
+
+# check zincsearch responding
+# localhost:9200 is the fake elasticsearch reverse proxy to zincsearch
+zinclivecheck=$(/usr/local/bin/curl -s "http://localhost:9200/" | jq -r .name )
 
 # if zincsearch is up, use curl to create a sample index using local json file
 if [ "$zinclivecheck" = "zinc" ]; then
 	echo "Creating a default zincsearch aggregate index"
-	/usr/local/bin/curl --retry 5 --retry-delay 5 --retry-all-errors --user "$mycreds" \
+	/usr/local/bin/curl --silent --retry 5 --retry-delay 5 --retry-all-errors --netrc \
 	  -X PUT --data-binary @/tmp/dmarc_aggregate.json http://localhost:9200/dmarc_aggregate | jq -r .acknowledged
 
 	echo "Creating a default zincsearch forensic index"
-	/usr/local/bin/curl --retry 5 --retry-delay 5 --retry-all-errors --user "$mycreds" \
+	/usr/local/bin/curl --silent --retry 5 --retry-delay 5 --retry-all-errors --netrc \
 	  -X PUT --data-binary @/tmp/dmarc_forensic.json http://localhost:9200/dmarc_forensic | jq -r .acknowledged
 else
 	echo "Cannot create index, zincsearch is not live"
 	exit 1
 fi
 
+# delete temporary files
+echo "Deleting temp index json files"
+rm -r /tmp/dmarc_aggregate.json
+rm -r /tmp/dmarc_forensic.json
+
 # create virtualenv
+echo "Creating virtual environment"
 sudo -u parsedmarc virtualenv /opt/parsedmarc/venv || true
 
 # activate virtualenv
+echo "Activating virtual environment"
 . /opt/parsedmarc/venv/bin/activate || true
 
 # install parsedmarc
+echo "Installing parsedmarc as user parsedmarc"
 sudo -u parsedmarc /opt/parsedmarc/venv/bin/pip install -U parsedmarc || true
 
 # deactivate the virtual environment
+echo "Deactivating the virtual environment"
 deactivate
 
 # make sure the process can write to log file
+echo "Creating log file"
 touch /var/log/python-parsedmarc.log
 chown parsedmarc:parsedmarc /var/log/python-parsedmarc.log
 
@@ -73,6 +84,7 @@ chown parsedmarc:parsedmarc /var/log/python-parsedmarc.log
 # safe(r) separator for sed
 sep=$'\001'
 
+echo "Creating parsedmarc.ini file"
 # copy in custom parsedmarc.ini file
 < "$TEMPLATEPATH/parsedmarc.ini.in" \
   sed "s${sep}%%imapserver%%${sep}$IMAPSERVER${sep}g" | \
@@ -96,13 +108,16 @@ chown parsedmarc /usr/local/etc/parsedmarc.ini
 # image is live
 
 # copy over the rc script
+echo "Creating parsedmarc rc script"
 cp -f "$TEMPLATEPATH/parsedmarc.rc.in" /usr/local/etc/rc.d/parsedmarc
 
 # set executable bit on rc file
 chmod +x /usr/local/etc/rc.d/parsedmarc
 
 # enable the service
+echo "Enabling parsedmarc service"
 service parsedmarc enable || true
 
 # start the service so long
+echo "Starting parsedmarc service"
 service parsedmarc start || true
