19a5c3969233d05fb60207cba9cdd0a240703199
Bretton Vine <bretton@hivemind.net>
2023-07-28 00:31:28 +0200

    Version bump for rebuild
    fix local_unbound dns resolution with missing parameters for access control

--- saltstack/saltstack.d/local/share/cook/bin/configure-local-unbound.sh
+++ saltstack/saltstack.d/local/share/cook/bin/configure-local-unbound.sh
@@ -19,6 +19,14 @@ stub-zone:
   stub-addr: 127.0.0.1@8600
 ' >/etc/unbound/conf.d/consul.conf
 
+# we need to enable this to allow queries
+# only seems to work in conf.d dir
+echo 'server:
+  interface: 0.0.0.0
+  access-control: 10.0.0.0/8 allow
+  access-control: 127.0.0.0/8 allow
+' >/etc/unbound/conf.d/server.conf
+
 service local_unbound enable || true
 sysrc local_unbound_forwarders="${DNSFORWARDERS:-none}" || true
 service local_unbound setup || true
