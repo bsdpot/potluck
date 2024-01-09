6e7485b041c3f704c7d92dff33332ddf184c9818
Bretton Vine <bretton@hivemind.net>
2024-01-09 21:34:41 +0200

    Version bump for rebuild
    Fix missing dollar sign

--- mastodon-s3/mastodon-s3.d/local/share/cook/bin/configure-mastodon.sh
+++ mastodon-s3/mastodon-s3.d/local/share/cook/bin/configure-mastodon.sh
@@ -127,7 +127,7 @@ echo "Creating .env.production"
   sed "s${sep}%%elasticport%%${sep}$SETELASTICPORT${sep}g" | \
   sed "s${sep}%%elasticuser%%${sep}$SETELASTICUSER${sep}g" | \
   sed "s${sep}%%elasticpass%%${sep}$SETELASTICPASS${sep}g" | \
-  sed "s${sep}%%deepltranslatekey%%${sep}$SETDEEPLKEY{sep}g" | \
+  sed "s${sep}%%deepltranslatekey%%${sep}$SETDEEPLKEY${sep}g" | \
   sed "s${sep}%%deepltranslateplan%%${sep}$SETDEEPLPLAN${sep}g" \
   > /usr/local/www/mastodon/.env.production
 
