8b8c6f707e26cd6ab35e314a7df8592b8234dcac
Bretton Vine <bretton@hivemind.net>
2024-03-03 21:42:49 +0200

    Version bump for new base image
    FBSD14 base image

--- rbldnsd/CHANGELOG.md
+++ rbldnsd/CHANGELOG.md
@@ -1,3 +1,10 @@
+0.13
+
+* Version bump for new base image
+* FBSD14 base image
+
+---
+
 0.12
 
 * Version bump for new base image
--- rbldnsd/rbldnsd.sh
+++ rbldnsd/rbldnsd.sh
@@ -127,8 +127,20 @@ pkg install -y rbldnsd
 step "Install package git-lite"
 pkg install -y git-lite
 
-step "Install package php82 and related"
-pkg install -y php82 php82-mbstring php82-zlib php82-curl php82-gd
+step "Install package php82"
+pkg install -y php82
+
+step "Install package php82-mbstring"
+pkg install -y php82-mbstring
+
+step "Install package php82-zlib"
+pkg install -y php82-zlib
+
+step "Install package php82-curl"
+pkg install -y php82-curl
+
+step "Install package php82-gd"
+pkg install -y php82-gd
 
 step "Install package syslog-ng"
 pkg install -y syslog-ng
