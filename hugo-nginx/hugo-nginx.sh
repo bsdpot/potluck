e199a37a35b8f0a109832397422485632639b711
Bretton Vine <bretton@hivemind.net>
2024-10-09 22:03:08 +0200

    Version bump for new base image 14.1
    Extra steps to trim image size

--- hugo-nginx/hugo-nginx.sh
+++ hugo-nginx/hugo-nginx.sh
@@ -86,6 +86,10 @@ service sshd enable || true
 step "Create /usr/local/etc/rc.d"
 mkdir -p /usr/local/etc/rc.d
 
+step "Clean freebsd-update"
+rm -rf /var/db/freebsd-update
+mkdir -p /var/db/freebsd-update
+
 # we need consul for consul agent
 step "Install package consul"
 pkg install -y consul
@@ -144,7 +148,7 @@ pkg install -y tmux
 
 step "Clean package installation"
 pkg autoremove -y
-pkg clean -y
+pkg clean -ay
 
 # -------------- END PACKAGE SETUP -------------
 
