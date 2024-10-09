2b73aee257ba9f884096b7d93f2fdf881c006c0b
Bretton Vine <bretton@hivemind.net>
2024-10-09 23:37:39 +0200

    Version bump for FBSD 14.1
    Extra steps to trim image size

--- opensearch/opensearch.sh
+++ opensearch/opensearch.sh
@@ -83,6 +83,10 @@ service sendmail onedisable
 step "Create /usr/local/etc/rc.d"
 mkdir -p /usr/local/etc/rc.d
 
+step "Clean freebsd-update"
+rm -rf /var/db/freebsd-update
+mkdir -p /var/db/freebsd-update
+
 # we need consul for consul agent
 step "Install package consul"
 pkg install -y consul
@@ -178,7 +182,7 @@ step "Package autoremove"
 pkg autoremove -y
 
 step "Clean package installation"
-pkg clean -y
+pkg clean -ay
 
 #
 # Now generate the run command script "cook"
