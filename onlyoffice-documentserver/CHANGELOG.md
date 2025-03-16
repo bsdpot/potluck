c7680485865b962359f11acbea1fab488a0fa27e
bretton <bretton@hivemind.net>
2025-03-16 20:54:17 +0200

    onlyoffice-documentserver: initiate flavour
    This is a development jail, more work may be required. Do not use in production yet.

---
+++ onlyoffice-documentserver/CHANGELOG.md
@@ -0,0 +1,3 @@
+0.0
+
+* First bash at a pot jail with onlyoffice-documentserver
---
+++ onlyoffice-documentserver/onlyoffice-documentserver.d/local/share/cook/bin/configure-database.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# shellcheck disable=SC1091
+if [ -e /root/.env.cook ]; then
+    . /root/.env.cook
+fi
+
+set -e
+# shellcheck disable=SC3040
+set -o pipefail
+
+export PATH="/usr/local/bin:$PATH"
+
+# function to check database exists
+check_database() {
+    /usr/local/bin/psql "postgresql://$DBUSER:$DBPASS@$DBHOST:$SETDBPORT/postgres" -lqt | grep -c "$DBNAME"
+}
+
+# unset these for dbcheck
+set +e
+# shellcheck disable=SC3040
+set +o pipefail
+
+# check if database exists
+dbcheck=$(check_database)
+
+# import database structure if db exists
+if [ "$dbcheck" -eq 1 ]; then
+    echo "Configuring onlyoffice database"
+    /usr/local/bin/psql "postgresql://$DBUSER:$DBPASS@$DBHOST:$SETDBPORT/$DBNAME" -f /usr/local/www/onlyoffice/documentserver/server/schema/postgresql/createdb.sql
+else
+    echo "Database $DBNAME does not exist, cannot continue"
+    exit 1
+fi
+
+# set back after dbcheck
+set -e
+# shellcheck disable=SC3040
+set -o pipefail
